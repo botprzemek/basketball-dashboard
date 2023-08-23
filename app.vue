2b2ab1b768aaa924cbc72302bf2e93d0b43ae800
botprzemek <botprzemekk@gmail.com>
2023-08-23 20:37:49 +0200

    Validation

--- app.vue
+++ app.vue
@@ -1,18 +1,35 @@
 <script setup>
-const url = 'http://localhost:3001/players'
-const { data: players, pending } = await useLazyFetch(url, {
+import Player from './models/Player'
+
+const players = []
+const { data, pending, error } = await useLazyFetch('http://localhost:3001/players', {
   headers: { secret: 'Rudzielec2208' },
-  transform: (players) => {
-    console.log(players.sort())
-  }
+  transform: response => response.map(player =>
+      players.push(new Player(
+          player.name,
+          player.lastname,
+          player.number,
+          player.height,
+          player.position
+      ).getPlayerData()
+    )
+  )
 })
+setTimeout(() => console.log(players), 1500)
 </script>
 
 <template>
-  <div>
-    <div v-if="!pending" v-for="player in players">
-      <h1>{{ player.name }}</h1>
-      <p>{{ player.last_name }}</p>
-    </div>
-  </div>
+  <main>
+    <section v-if="pending">
+      <h1>Ładuję...</h1>
+      {{ players.length }}
+    </section>
+    <section v-else v-for="player in players" :key="player.lastname">
+      <h1>{{ player.lastname }}</h1>
+      <p>{{ player.name }}</p>
+      <p>{{ player.number }}</p>
+      <p>{{ `${player.height.in}`.replaceAll('.', "'") }}</p>
+      <p>{{ player.position.short }}</p>
+    </section>
+  </main>
 </template>
