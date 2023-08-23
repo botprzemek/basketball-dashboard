de97402197422c2f37b244d7948344446c441db0
botprzemek <info@botprzemek.pl>
2023-08-23 06:27:17 +0200

    Fetch data

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
+<script setup>
+const url = 'http://localhost:3001/players'
+const { data: players, pending } = await useLazyFetch(url, {
+  headers: { secret: 'Rudzielec2208' },
+  transform: (players) => {
+    console.log(players.sort())
+  }
+})
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <div v-if="!pending" v-for="player in players">
+      <h1>{{ player.name }}</h1>
+      <p>{{ player.last_name }}</p>
+    </div>
   </div>
 </template>
