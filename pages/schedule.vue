52703bfa5a7c1ce844f98e08df05da87af8e379a
botprzemek <botprzemekk@gmail.com>
2023-10-10 21:48:57 +0200

    Added player endpoint for nuxt api

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -1,16 +1,53 @@
-<script setup lang="ts">
+<script setup>
+import { io } from 'socket.io-client'
+import { game } from '~/utils/game.util'
+
+const socket = io('http://localhost:3001')
 const { t } = useI18n({ useScope: 'global' })
 
 useHead({
   title: t(`path.${useRouter().currentRoute.value.name.split('___')[0]}`)
 })
+
+onMounted(() => {
+  game(socket)
+})
 </script>
 
 <template>
   <h1>{{ $t(`path.${$route.name.split('___')[0]}`) }}</h1>
+
   ŚLK
   Sparing
   Wyniki
   Statystyki meczów
   Hala
+
+  <h1>Basketball Game</h1>
+  <p>Game Time: <span id="gameTime">00:00</span></p>
+  <p>Quarter: <span id="quarter">1</span></p>
+  <p>Status: <span id="status">End of regulation</span></p>
+  <p>Paused: <span id="paused">Paused</span></p>
+
+  <h3>Host</h3>
+  <p>Points: <span class="scoreHost">0</span></p>
+
+  <h3>Opponent</h3>
+  <p>Points: <span class="scoreOpponent">0</span></p>
+
+  <h2>Admin</h2>
+
+  <h3>Host</h3>
+  <button class="changeScoreHost" value="1">1pt</button>
+  <button class="changeScoreHost" value="2">2pt</button>
+  <button class="changeScoreHost" value="3">3pt</button>
+
+  <h3>Opponent</h3>
+  <button class="changeScoreOpponent" value="1">1pt</button>
+  <button class="changeScoreOpponent" value="2">2pt</button>
+  <button class="changeScoreOpponent" value="3">3pt</button>
+
+  <h3>Time</h3>
+  <button id="changeStatus">Change Status</button>
+  <button id="pauseGame">Pause</button>
 </template>
---
+++ utils/game.util.ts
@@ -0,0 +1,63 @@
+export const format = (sec): string => {
+    if (!sec) return '00:00'
+    const min: number = Math.floor(sec / 60)
+    const secLeft: number = sec % 60
+    const newMin: string = min < 10 ? `0${min}` : `${min}`
+    const newSec: string = secLeft < 10 ? `0${secLeft}` : `${secLeft}`
+
+    return `${newMin}:${newSec}`
+}
+
+export const game = (socket): void => {
+    socket.on("connect_error", (err): void => {
+        console.log(`connection error due to ${err.message}`);
+    });
+
+    socket.on('initialData', (data): void => {
+        document.getElementById('gameTime').textContent = format(data.time)
+        document.getElementById('quarter').textContent = data.quarter ? data.quarter : 'End of regulation'
+        document.getElementById('status').textContent = data.status ? 'Started' : 'Ended'
+    })
+
+    socket.on('updateData', (data): void => {
+        document.querySelector('.scoreHost').textContent = data.scoreHost
+        document.querySelector('.scoreOpponent').textContent = data.scoreOpponent
+        document.getElementById('gameTime').textContent = format(data.time)
+        document.getElementById('quarter').textContent = data.quarter ? data.quarter : 'End of regulation'
+        document.getElementById('status').textContent = data.status ? 'Started' : 'Ended'
+    })
+
+    socket.on('updateScore', (data): void => {
+        document.querySelector('.scoreHost').textContent = data.scoreHost
+        document.querySelector('.scoreOpponent').textContent = data.scoreOpponent
+    })
+
+    socket.on('updateTimer', (data): void => {
+        document.getElementById('gameTime').textContent = format(data.time)
+        document.getElementById('quarter').textContent = data.quarter ? data.quarter : 4
+        document.getElementById('status').textContent = data.status ? 'Started' : 'End of regulation'
+        document.getElementById('paused').textContent = data.paused ? 'Paused' : 'Playing'
+    })
+
+    document.getElementById('changeStatus').addEventListener('click', (): void => {
+        if (document.getElementById('status').textContent === 'End of regulation') return
+        socket.emit('changeStatus')
+    })
+
+    document.getElementById('pauseGame').addEventListener('click', (): void => {
+        if (document.getElementById('status').textContent !== 'Started') return
+        socket.emit('pauseGame')
+    })
+
+    document.querySelectorAll('.changeScoreHost').forEach((element) =>
+        element.addEventListener('click', (): void => {
+            socket.emit('updateScore', { scoreHost: element.value, scoreOpponent: 0 })
+        }),
+    )
+
+    document.querySelectorAll('.changeScoreOpponent').forEach((element) =>
+        element.addEventListener('click', (): void => {
+            socket.emit('updateScore', { scoreHost: 0, scoreOpponent: element.value })
+        }),
+    )
+}
